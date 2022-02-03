d937be8fac2364fbe87a9d8d415547920e0ae55d
ian <kickthechair@gmail.com>
2022-02-03 18:59:51 +0000

    Sinatra::Reloader working (don't have to manually restart local server to see changes. Added a couple more routes for testing purposes

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
   "Hello!"
+end
+
+get '/secret' do
+  "I see dead people"
+end
+
+get '/reloader_test' do
+  "Sinatra::Reloader works!"
 end
