9924cab56d8777326c63be5c986444575b521917
ian <kickthechair@gmail.com>
2022-02-07 17:04:51 +0000

    Initial project setup and rackup working correctly. Run the server with command 'rackup' on port: 9292

--- app.rb
+++ app.rb
@@ -1,3 +1,13 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    "Hello Battle!"
+  end
+
+end
