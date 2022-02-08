4528ac56bb0f18c28df3bf9794de46dccb1ad467
ian <kickthechair@gmail.com>
2022-02-08 11:34:46 +0000

    modified app.rb to setup a POST/Redirect/Get when submitting the player names via the form. Also setup a session to persist state between HTTP requests, allowing user names to be available in the new '/play' route

--- app.rb
+++ app.rb
@@ -6,13 +6,21 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params[:player1]
-    @player_2_name = params[:player2]
+    session[:player_1_name] = params[:player1]
+    session[:player_2_name] = params[:player2]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb(:play)
   end
 
