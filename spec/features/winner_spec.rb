f45c8b2b12c21fc9c64abacc565cfa07a0d0351e
ian <kickthechair@gmail.com>
2022-02-12 15:09:57 +0000

    Player 2 now gets a win message if they win

--- spec/features/winner_spec.rb
+++ spec/features/winner_spec.rb
@@ -8,4 +8,12 @@ feature 'if a players HP reaches 0 announce a winner' do
     expect(page).to have_content('Dave is the Winner!!!')
   end
 
+  scenario 'player 2 beats player 1' do
+    sign_in_and_play
+    5.times do
+      click_button('Jess attacks Dave')
+    end
+    expect(page).to have_content('Jess is the Winner!!!')
+  end
+
 end
